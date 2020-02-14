73badd2c21d8126acc3e14c29462d5aaa467419e
Pearl Joy Abenir <pdabenir@up.edu.ph>
2020-02-14 10:34:47 +0800

    Update abenirBC.js

--- abenirBC.js
+++ abenirBC.js
@@ -1,14 +1,25 @@
 function convert() {
 	var num = Math.trunc(Number(document.getElementById("number").value));
 	var x = num;
+	var rem;
 	var array = [];
+	
 	
 	while (x != 0){
 		rem = x%2;
 		x = Math.trunc(x/2);
 		array.push(rem);
 	}
-
+	
+	
 	document.getElementById("result").innerHTML = "Decimal Number: " + num + "<br>Binary form: ";
-	while (array.length != 0) document.getElementById("result").innerHTML += array.pop();
+	
+	if (array.length == 0){
+		document.getElementById("result").innerHTML = "Decimal Number: " + num + "<br>Binary form: 0 ";
+	}
+	else{
+		while (array.length != 0){
+		document.getElementById("result").innerHTML += array.pop();
+		}
+	}
 }
